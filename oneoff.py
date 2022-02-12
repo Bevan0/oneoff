95f7039bbe66ac14227a35c2667ab8f956e1f225
Bevan <bevanellis8@gmail.com>
2022-02-12 16:09:49 +1300

    Reopen last by setting runner to !

--- oneoff.py
+++ oneoff.py
@@ -2,6 +2,7 @@ import click
 import subprocess
 import random
 import string
+import os
 
 @click.group()
 def cli(): pass
@@ -10,13 +11,22 @@ def cli(): pass
 @click.argument("runner")
 @click.option("--editor", default="nano")
 def run(runner, editor):
-    file_name = f"/tmp/oneoff-{''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))}"
-    f = open(file_name, "w")
-    f.close()
+    file_name = None
+    realrunner = runner
+    if runner == "!":
+        prevfile = open(f"{os.environ['HOME']}/.config/oneoff-previousfile", "r")
+        file_name = prevfile.read().split('\n')[1]
+        realrunner = prevfile.read().split('\n')[0]
+        prevfile.close()
+    else:
+        file_name = f"/tmp/oneoff-{''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))}"
 
     edit = subprocess.run([f"{editor}", f"{file_name}"])
     if (edit.returncode != 0):
         click.echo("Editor returned non-zero error code, quitting")
         return
     
-    subprocess.run([f"{runner}", f"{file_name}"])
+    subprocess.run([f"{realrunner}", f"{file_name}"])
+
+    f = open(f"{os.environ['HOME']}/.config/oneoff-previousfile", "w")
+    f.write(f"{runner}\n{file_name}")
